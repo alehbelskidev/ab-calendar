f8ef642088848244577640b3bbee8bcd2c233af6
Aleh Belski <aleh.belski@absoftware.dev>
2025-11-21 21:02:47 +0100

    feat: adding precommit hook, setting padding to preview container

--- src/App.tsx
+++ src/App.tsx
@@ -21,42 +21,46 @@ import {
 
 function App() {
 	return (
-		<CalendarProvider>
-			<CalendarRoot>
-				<CalendarHeader>
-					<CalendarDateTitle />
-					<CalendarHeaderNav>
-						<CalendarHeaderNavPrevButton />
-						<CalendarHeaderNavNextButton />
-					</CalendarHeaderNav>
-					<CalendarViewSwitcher>
-						<CalendarViewSwitcherButton view="month">
-							Month
-						</CalendarViewSwitcherButton>
-						<CalendarViewSwitcherButton view="week">
-							Week
-						</CalendarViewSwitcherButton>
-						<CalendarViewSwitcherButton view="day">
-							Day
-						</CalendarViewSwitcherButton>
-					</CalendarViewSwitcher>
-					<CalendarTodayButton>Today</CalendarTodayButton>
-				</CalendarHeader>
-				<CalendarContent>
-					<CalendarMonthView>
-						<CalendarMonthHeader
-							onHeaderCellClick={(date) => console.log(date)}
-						/>
-						<CalendarMonthGrid>
-							<CalendarMonthCellButton onClick={(date) => console.log(date)} />
-						</CalendarMonthGrid>
-					</CalendarMonthView>
-					<CalendarWeekView />
-					<CalendarDayView />
-				</CalendarContent>
-				<CalendarFooter>Footer</CalendarFooter>
-			</CalendarRoot>
-		</CalendarProvider>
+		<div className="min-h-screen w-full p-4">
+			<CalendarProvider>
+				<CalendarRoot>
+					<CalendarHeader>
+						<CalendarDateTitle />
+						<CalendarHeaderNav>
+							<CalendarHeaderNavPrevButton />
+							<CalendarHeaderNavNextButton />
+						</CalendarHeaderNav>
+						<CalendarViewSwitcher>
+							<CalendarViewSwitcherButton view="month">
+								Month
+							</CalendarViewSwitcherButton>
+							<CalendarViewSwitcherButton view="week">
+								Week
+							</CalendarViewSwitcherButton>
+							<CalendarViewSwitcherButton view="day">
+								Day
+							</CalendarViewSwitcherButton>
+						</CalendarViewSwitcher>
+						<CalendarTodayButton>Today</CalendarTodayButton>
+					</CalendarHeader>
+					<CalendarContent>
+						<CalendarMonthView>
+							<CalendarMonthHeader
+								onHeaderCellClick={(date) => console.log(date)}
+							/>
+							<CalendarMonthGrid>
+								<CalendarMonthCellButton
+									onClick={(date) => console.log(date)}
+								/>
+							</CalendarMonthGrid>
+						</CalendarMonthView>
+						<CalendarWeekView />
+						<CalendarDayView />
+					</CalendarContent>
+					<CalendarFooter>Footer</CalendarFooter>
+				</CalendarRoot>
+			</CalendarProvider>
+		</div>
 	)
 }
 
