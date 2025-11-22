0295450c33fdd42554ca3e0b74bd99d50104a1dc
Aleh Belski <aleh.belski@absoftware.dev>
2025-11-22 18:04:26 +0100

    fix: frogotten from prev commit

--- src/calendar/content/calendar-time-col.tsx
+++ src/calendar/content/calendar-time-col.tsx
@@ -1,6 +1,6 @@
+import dayjs from "dayjs"
 import { type HTMLAttributes, useMemo } from "react"
 import { cn } from "@/lib/utils"
-import dayjs from "dayjs"
 
 type CalendarTimeColProps = HTMLAttributes<HTMLDivElement> & {
 	colStart?: number
